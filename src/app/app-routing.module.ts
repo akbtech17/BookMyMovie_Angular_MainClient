f77cce87f9ec41f14abba3d3a29f13f574d868df
anshul bansal <akbt.tech17@gmail.com>
2022-09-21 21:06:28 +0530

    created basic components and routing for them

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RegisterComponent } from './register/register.component';
+import { SigninComponent } from './signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signin', component: SigninComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'movies-list', component: MoviesListComponent },
+  { path: 'movie-details', component: MovieDetailsComponent },
+  
+  // default route 
+  {path: '', redirectTo: 'signin', pathMatch: 'full' },
+
+  // wildcard route
+  {path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { RegisterComponent } from './register/register.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    RegisterComponent,
+    MoviesListComponent,
+    MovieDetailsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
