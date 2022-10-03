c4760a518ba2b51243e0d5e8613e9d41847cf390
anshul bansal <akbt.tech17@gmail.com>
2022-10-04 01:23:53 +0530

    doing movie list component

--- src/app/Admin/admin-movielist/admin-movielist.component.ts
+++ src/app/Admin/admin-movielist/admin-movielist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IMovie } from 'src/app/movie';
+import { MovieService } from 'src/app/movie.service';
 
 @Component({
   selector: 'app-admin-movielist',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-movielist.component.css']
 })
 export class AdminMovielistComponent implements OnInit {
-
-  constructor() { }
+  movies: IMovie[] = []
+  constructor(private movieService: MovieService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.movieService.getMovieList().subscribe( data => {
+      this.movies = data
+    })
   }
 
 }
