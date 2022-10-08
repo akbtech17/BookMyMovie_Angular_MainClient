84b3adce0e61d93399ff881a1541324829d0f734
anshul bansal <akbt.tech17@gmail.com>
2022-10-08 09:55:42 +0530

    seat book component is almost ready

--- src/app/seat-booking/seat-booking.component.ts
+++ src/app/seat-booking/seat-booking.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { MovieService } from '../movie.service';
 
 @Component({
@@ -7,19 +9,31 @@ import { MovieService } from '../movie.service';
   styleUrls: ['./seat-booking.component.css']
 })
 export class SeatBookingComponent implements OnInit {
-
   // 0 - not selected
   // 1 - selected
   // 2 - booked
-
   seatMap?: any[];
-  constructor(private movieService: MovieService) { }
+  selectedSeats?: string[] = [];
+  constructor(private movieService: MovieService, private toastr: ToastrService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   onClick(seatNo: string) {
-    console.log(seatNo);
     if(this.seatMap) {
       this.seatMap.forEach(seat => {
-        if(seat.status!=2 && seat.seatNo == seatNo) seat.status = seat.status == 0 ? 1 : 0
+        if(seat.status!=2 && seat.seatNo == seatNo) {
+          // seat.status = seat.status == 0 ? 1 : 0
+          if(seat.status == 0) {
+            seat.status = 1;
+            this.toastr.success('',`Seat ${seatNo}`, {
+              timeOut: 3000,
+            });
+          }
+          else {
+            seat.status = 0;
+            this.toastr.error('',`Seat ${seatNo}`, {
+              timeOut: 3000,
+            });
+          }
+        }
       });
     }
   }
@@ -33,5 +47,24 @@ export class SeatBookingComponent implements OnInit {
         console.log(err)
       }
     )
+  }
+
+  OnSubmit() {
+    this.selectedSeats = [];
+      if(this.seatMap) {
+        this.seatMap.forEach(seat => {
+          if(seat.status == 1) this.selectedSeats?.push(seat.seatNo);
+        });
+      }
+      if(this.selectedSeats.length == 0){
+        this.toastr.error(`Please select some seats to proceed`,`No seat is selected`, {
+          timeOut: 3000,
+        });
+      }
+      else {
+        this.toastr.success(`Booking ${this.selectedSeats} ${this.selectedSeats.length == 1 ? 'seat' : 'seats'} for you`,`Processing your request`, {
+          timeOut: 3000,
+        });
+      }
   }
 }
