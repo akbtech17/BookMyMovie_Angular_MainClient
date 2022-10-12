6b022a7c16f86d98cf0307005cecc6ecf278aef5
anshul bansal <akbt.tech17@gmail.com>
2022-10-13 00:53:37 +0530

    changed backend string

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://anshulkumarapi246.azurewebsites.net/api/'
+  baseUrl: 'https://bookmymoviedotnetwebapi.azurewebsites.net/api/'
 };
