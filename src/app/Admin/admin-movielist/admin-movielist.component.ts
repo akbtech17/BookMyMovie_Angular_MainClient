ab87f6520232ea8a5b386f37ccb55cf68dbeea4f
anshul bansal <akbt.tech17@gmail.com>
2022-10-12 22:54:01 +0530

    movie date fixed and alignment too

--- src/app/Admin/admin-movielist/admin-movielist.component.ts
+++ src/app/Admin/admin-movielist/admin-movielist.component.ts
@@ -12,6 +12,16 @@ export class AdminMovielistComponent implements OnInit {
   movies: IMovie[] = []
   constructor(private movieService: MovieService, private activatedRoute: ActivatedRoute) { }
 
+  formatDate(dateTime: string): string {
+    const months = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const date = dateTime.substring(0,10);
+    const userEnteredDateArray = date.split('-');
+    let yearValue = userEnteredDateArray[0];
+    let monthValue = userEnteredDateArray[1];
+    let dateValue = userEnteredDateArray[2];
+    return dateValue + ' ' + months[parseInt(monthValue)] + ' ' + yearValue;
+  }
+
   ngOnInit(): void {
     this.movieService.getMovieList().subscribe( data => {
       this.movies = data
